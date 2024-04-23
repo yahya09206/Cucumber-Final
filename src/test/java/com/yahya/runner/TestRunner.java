855c8347d9cc8a1614fdc9ac09b7202f9a8a1164
Yahya <yahyahussein91@gmail.com>
2024-04-23 16:53:22 -0700

    verified hooks class is closing browser after tests

--- src/test/java/com/yahya/runner/TestRunner.java
+++ src/test/java/com/yahya/runner/TestRunner.java
@@ -1,10 +1,12 @@
 package com.yahya.runner;
 
 import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 
 @RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources/features",
+        glue = "com/yahya/step_definitions")
 public class TestRunner {
-
 }
