8ea0d5d9f0308e4ec414f5bdd4170fd97edf4e8b
Yahya <yahyahussein91@gmail.com>
2024-04-24 11:40:18 -0700

    shortened class path for finding the feature files

--- src/test/java/com/yahya/runner/TestRunner.java
+++ src/test/java/com/yahya/runner/TestRunner.java
@@ -6,8 +6,8 @@ import org.junit.runner.RunWith;
 
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features",
-        glue = "com/yahya/step_definitions",
+@CucumberOptions(features = "classpath:features"//"//src/test/resources/features"
+        ,glue = "com/yahya/step_definitions",
         plugin = {"html:target/cucumber_report.html"},
         dryRun = false,
         tags = "@wip"
