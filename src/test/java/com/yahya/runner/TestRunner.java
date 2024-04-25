8ce2fb9e46ad5297da09dd7f5224da22906d1e6d
Yahya <yahyahussein91@gmail.com>
2024-04-25 11:23:40 -0700

    could not add third party report plugin

--- src/test/java/com/yahya/runner/TestRunner.java
+++ src/test/java/com/yahya/runner/TestRunner.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
         plugin = {"pretty", "html:target/cucumber_report.html",
                   "rerun:target/rerun.txt"},
         dryRun = false,
-        tags = "@failed"
-        )
+        tags = ""
+)
 public class TestRunner {
 }
